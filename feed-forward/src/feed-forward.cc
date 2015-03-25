22323d409dbbb3bcb0e06a3bba9fad54011d2081
Tudor Berariu <tudor.berariu@gmail.com>
2015-03-25 15:56:15 +0200

    Added abstract Layer structure

--- feed-forward/src/feed-forward.cc
+++ feed-forward/src/feed-forward.cc
@@ -16,23 +16,21 @@ int main(int argc, char* argv[])
 {
   feenableexcept(FE_INVALID | FE_OVERFLOW);
   using Mnist = MNIST<long double>;
-  using NN = Layer<long double, Mnist::D, 300, Mnist::K>;
+  using NN = FeedForwardLayer<long double, Mnist::D, 300, Mnist::K>;
   Mnist* mnist;
   readMnistDataSet(argv[1], mnist);
 
-  cout << "done" << endl;
   NN nn;
-  for (int i = 0; i < 20; i++) {
-    auto out = nn.forward(mnist->xTrain[1]);
+  for (int i = 0; i < 2; i++) {
+    auto out = nn.forward(mnist->xTrain[0]);
     long double error = 0.0;
     for (auto j = 0; j < 10; j++) {
-      error += (out[j] - mnist->tTrain[1][j]) * (out[j] - mnist->tTrain[1][j]);
+      error += (out[j] - mnist->tTrain[0][j]) * (out[j] - mnist->tTrain[0][j]);
     }
     cout << error << endl;
     nn.backpropagate(mnist->xTrain[0], mnist->tTrain[0]);
     nn.adjust(0.5);
   }
-
   delete(mnist);
   return 0;
 }
--- feed-forward/src/nn/feedforward.h
+++ feed-forward/src/nn/feedforward.h
@@ -16,12 +16,13 @@ using namespace std;
 
 template <typename T>
 inline T sigmoid(const T x) {
-  feenableexcept(0);
+  /*
   const T e = exp(-x);
-  feenableexcept(FE_INVALID | FE_OVERFLOW);
   return (std::isnan(e) ?
           nexttoward(1.0, 0.0) :
           (std::isinf(e) ? nexttoward(0.0, 1.0) : (1.0 / (1.0 + e))));
+  */
+  return 1.0 / (1.0 + exp(-x));
 }
 
 template <typename T>
@@ -29,28 +30,20 @@ inline T derivate(const T s) {
   return s * (1 - s);
 }
 
-template<typename T, size_t... Ms>
-struct Layer;
-
-template<typename T, size_t Min, size_t Mout>
-struct Layer<T, Min, Mout> {
-public:
+template <typename T, size_t Min, size_t Mout>
+struct Layer {
+ public:
+  using Connections = array<array<T, Min+1>, Mout>;
   using In = array<T, Min>;
   using Out = array<T, Mout>;
-  using Connections = array<array<T, Min+1>, Mout>;
-  using NetOutput = Out;
+
+  Connections weights;
+  Connections gradient;
 
   Out activations;
   Out outputs;
   Out error;
-  Connections gradient;
 
-  static const size_t M = Mout;
-  static const size_t K = Mout;
-
-  Connections weights;
-
-  // initialize the layers
   Layer() {
     random_device rd;
     default_random_engine e(rd());
@@ -62,101 +55,23 @@ public:
     }
   }
 
-  // forward computation
-  const NetOutput& forward(const In& x) {
+  void updateStates(const In& x) {
     for (size_t j = 0; j < Mout; j++) {
       activations[j] = weights[j][Min];
       for (size_t i = 0; i < Min; i++) {
         activations[j] += weights[j][i] * x[i];
       }
-      outputs[j] = sigmoid<T>(activations[j]);
-    }
-    return outputs;
-  }
-
-  // error backpropagation
-  const Out& backpropagate(const In& x, const NetOutput& t) {
-    for (size_t j = 0; j < K; j++) {
-      error[j] = (outputs[j] - t[j]) * derivate(activations[j]);
-      for (size_t i= 0; i < Min; i++) {
-        gradient[j][i] = error[j] * x[i];
-      }
-      gradient[j][Min] = error[j];
-    }
-    return error;
-  }
-
-  void adjust(const T learningRate) {
-    for (size_t j = 0; j < Mout; j++) {
-      for (size_t i = 0; i <= Min; i++) {
-        weights[j][i] -= gradient[j][i] * learningRate;
-      }
+      outputs[j] = sigmoid(activations[j]);
     }
   }
 
-  void print(ostream& os) const {
-    os << "--------------------" << endl;
-    for (size_t j = 0; j < Mout; j++) {
-      for (size_t i = 0; i <= Min; i++) {
-        os << weights[j][i] << " ";
-      }
-      os << endl;
-    }
-    os << "--------------------" << endl;
-  }
-};
-
-template<typename T, size_t Min, size_t Mout, size_t... Ms>
-struct Layer<T, Min, Mout, Ms...> {
-public:
-  using NextLayer = Layer<T, Mout, Ms...>;
-  using In = array<T, Min>;
-  using Out = array<T, Mout>;
-  using Connections = array<array<T, Min+1>, Mout>;
-  using NetOutput = typename NextLayer::NetOutput;
-
-  static const size_t K = NextLayer::K;
-  static const size_t M = Mout;
-
-  NextLayer nextLayer;
-
-  Out activations;
-  Out outputs;
-
-  Out error;
-  Connections gradient;
-
-  Connections weights;
-
-  Layer() : nextLayer { } {
-    random_device rd;
-    default_random_engine e(rd());
-    uniform_real_distribution<T> next(-0.05, 0.05);
-    for (size_t j = 0; j < Mout; j++) {
-      for (size_t i = 0; i <= Min; i++) {
-        weights[j][i] = next(e);
-      }
-    }
-  }
-
-  const NetOutput& forward(const In& x) {
-    for (size_t j = 0; j < Mout; j++) {
-      activations[j] = weights[j][Min];
-      for (size_t i = 0; i < Min; i++) {
-        activations[j] += weights[j][i] * x[i];
-      }
-      outputs[j] = sigmoid<T>(activations[j]);
-    }
-    return nextLayer.forward(outputs);
-  }
-
-  const Out& backpropagate(const In& x, const NetOutput& t) {
-    const array<T, NextLayer::M>& nextError =
-      nextLayer.backpropagate(outputs, t);
+  template <size_t Mnext>
+  const Out& backpropagate(const In& x, const array<T, Mnext>& nextError,
+                           const array<array<T, Mout+1>, Mnext>& nextWeights) {
     for (size_t j = 0; j < Mout; j++) {
       error[j] = 0;
-      for (size_t k = 0; k < nextLayer.M; k++) {
-        error[j] += nextLayer.weights[k][j] * nextError[k];
+      for (size_t k = 0; k < Mnext; k++) {
+        error[j] += nextWeights[k][j] * nextError[k];
       }
       error[j] *= derivate(activations[j]);
       for (size_t i = 0; i < Min; i++) {
@@ -183,7 +98,80 @@ public:
       }
       os << endl;
     }
-    os << " --- " << endl;
+    os << "--------------------" << endl;
+  }
+};
+
+template<typename T, size_t... Ms>
+struct FeedForwardLayer;
+
+template<typename T, size_t Min, size_t Mout>
+struct FeedForwardLayer<T, Min, Mout> : Layer<T, Min, Mout> {
+public:
+  using Neurons = Layer<T, Min, Mout>;
+  using Connections = array<array<T, Min+1>, Mout>;
+  using NetOutput = typename Neurons::Out;
+  using In = typename Neurons::In;
+  using Out = typename Neurons::Out;
+
+  static const size_t M = Mout;
+  static const size_t K = Mout;
+
+  FeedForwardLayer() { }
+
+  // forward computation
+  const NetOutput& forward(const In& x) {
+    Neurons::updateStates(x);
+    return Neurons::outputs;
+  }
+
+  // error backpropagation
+  const Out& backpropagate(const In& x, const NetOutput& t) {
+    for (size_t j = 0; j < K; j++) {
+      Neurons::error[j] = (Neurons::outputs[j] - t[j])
+        * derivate(Neurons::activations[j]);
+      for (size_t i= 0; i < Min; i++) {
+        Neurons::gradient[j][i] = Neurons::error[j] * x[i];
+      }
+      Neurons::gradient[j][Min] = Neurons::error[j];
+    }
+    return Neurons::error;
+  }
+
+  void print(ostream& os) const {
+    Neurons::print(os);
+  }
+};
+
+template<typename T, size_t Min, size_t Mout, size_t... Ms>
+struct FeedForwardLayer<T, Min, Mout, Ms...> : Layer<T, Min, Mout>  {
+public:
+  using Neurons = Layer<T, Min, Mout>;
+  using NextLayer = FeedForwardLayer<T, Mout, Ms...>;
+  using In = typename Neurons::In;
+  using Out = typename Neurons::Out;
+  using NetOutput = typename NextLayer::NetOutput;
+
+  static const size_t K = NextLayer::K;
+  static const size_t D = Min;
+  static const size_t M = Mout;
+
+  NextLayer nextLayer;
+
+  const NetOutput& forward(const In& x) {
+    Neurons::updateStates(x);
+    return nextLayer.forward(Neurons::outputs);
+  }
+
+  const Out& backpropagate(const In& x, const NetOutput& t) {
+    Neurons::backpropagate(x,
+                           nextLayer.backpropagate(Neurons::outputs, t),
+                           nextLayer.weights);
+    return Neurons::error;
+  }
+
+  void print(ostream& os) const {
+    Neurons::print(os);
     nextLayer.print(os);
   }
 };
